59de142d67e52bc80959704ea3a0b652fd22cb16
zhanghaoran <240964503@qq.com>
2020-06-04 16:44:31 +0800

    create命令流程初步完成

--- src/main.js
+++ src/main.js
@@ -1,103 +1,66 @@
-import program from 'commander';
-import create from './create'; // 项目创建
-import init from './init'; // 项目初始化
-import dev from './dev'; // 项目启动
-import build from './build'; //项目打包
+import program from "commander";
+import symbol from "log-symbols";
+import chalk from "chalk";
+import ora from "ora";
 
-/**
- * little-bird-cli 命令列表
- */
-let actionMap = {
-  // 项目创建
-  create: {
-    description: '创建一个新的项目', // 描述
-    usages: [
-      // 使用方法
-      'little-bird-cli create ProjectName',
-      'lb-cli create ProjectName',
-      'lbc create ProjectName',
-    ],
-    alias: 'c', // 命令简称
-  },
-  // 项目初始化
-  init: {
-    description: '初始化项目',
-    usages: ['little-bird-cli init', 'lb-cli init', 'lbc init'],
-    alias: 'i',
-  },
-  // 启动项目
-  dev: {
-    description: '本地启动项目',
-    usages: ['little-bird-cli dev', 'lb-cli dev', 'lbc dev'],
-    options: [
-      {
-        flags: '-p --port <port>',
-        description: '端口',
-        defaultValue: 3000,
-      },
-    ],
-    alias: 'd',
-  },
-  //打包
-  build: {
-    description: '服务端项目打包',
-    usages: ['little-bird-cli build', 'lb-cli build', 'lbc build'],
-    options: [
-      {
-        flags: '-u --username <port>',
-        description: 'github用户名',
-        defaultValue: '',
-      },
-      {
-        flags: '-t --token <port>',
-        description: 'github创建的token',
-        defaultValue: '',
-      },
-    ],
-    alias: 'b',
-  },
-};
+import {
+  notExistFold,
+  prompt,
+  TEMPLATE_LIST,
+  downloadTemplate,
+  updateJsonFile,
+} from "./util";
 
-// 添加create,init,dev命令
-Object.keys(actionMap).forEach((action) => {
-  if (actionMap[action].options) {
-    Object.keys(actionMap[action].options).forEach((option) => {
-      let obj = actionMap[action].options[option];
-      program.option(obj.flags, obj.description, obj.defaultValue);
-    });
-  }
+program
+  .command("create")
+  .description("创建一个新项目")
+  .alias("c")
+  .action(async () => {
+    let loading = ora("模板下载中...");
+    const prjName = process.argv[3];
+    try {
+      if (prjName === undefined) {
+        console.log(symbol.error, chalk.red("创建项目时，请输入项目名"));
+      } else {
+        // 检测是否有同名文件
+        await notExistFold(prjName);
+        let promptResult = await prompt();
+        let dowloadUrl = ""; // 下载地址
 
-  // 通过绑定操作处理程序实现命令 (这里description的定义和 `.command` 是分开的)
-  // 返回新生成的命令(即该子命令)以供继续配置
-  program
-    .command(action)
-    .description(actionMap[action].description)
-    .alias(actionMap[action].alias)
-    .action(() => {
-      switch (action) {
-        // 到这里具体命令实现逻辑还空缺，我们先打日志，看下命令处理情况
-        case 'create':
-          // 获取当前的项目名
-          create(...process.argv.slice(3));
-          break;
-        case 'init':
-          init(program.username, program.token);
-          break;
-        case 'dev':
-          dev(program.port);
-          break;
-        case 'build':
-          build();
-          break;
-        default:
-          break;
+        switch (promptResult.frame) {
+          case TEMPLATE_LIST.react:
+            console.log(symbol.warning, chalk.yellow("react模板暂时没有"));
+            process.exit(1);
+            break;
+          case TEMPLATE_LIST.vue:
+            dowloadUrl = "https://github.com/EEEVO/zlwy.git";
+            break;
+          case TEMPLATE_LIST.miniApp:
+            break;
+          default:
+            break;
+        }
+
+        loading.start("模板下载中...");
+        await downloadTemplate(prjName, dowloadUrl);
+        loading.succeed("模板下载完成");
+        // TODO:此处需要throw
+        //   loading.succeed("模板下载失败");
+
+        // 下载完成后,根据用户输入更新配置文件
+        const fileName = `${prjName}/package.json`;
+        promptResult.name = prjName;
+        await updateJsonFile(fileName, promptResult);
+        console.log(symbol.success, chalk.green("配置文件更新完成"));
       }
-    });
-});
+    } catch (error) {
+      console.log(symbol.error, chalk.green(error));
+    }
+  });
 
 // 项目版本
 program
-  .version(require('../package.json').version, '-v --version')
+  .version(require("../package.json").version, "-v --version")
   .parse(process.argv);
 
 /**
--- src/util.js
+++ src/util.js
@@ -1,19 +1,19 @@
-import fs from 'fs';
-import symbol from 'log-symbols';
-import chalk from 'chalk';
-import inquirer from 'inquirer';
-import downloadGit from 'download-git-repo';
+import fs from "fs";
+import symbol from "log-symbols";
+import chalk from "chalk";
+import inquirer from "inquirer";
+import downloadGit from "download-git-repo";
 
 /**
  * 文件是否存在
  * @param {string} name
  */
-let notExistFold = async (name) => {
+export const notExistFold = async (name) => {
   return new Promise((resolve) => {
     if (fs.existsSync(name)) {
       console.log(
         symbol.error,
-        chalk.red('文件夹名已被占用，请更换名字重新创建')
+        chalk.red("文件夹名已被占用，请更换名字重新创建")
       );
     } else {
       resolve();
@@ -21,27 +21,38 @@ let notExistFold = async (name) => {
   });
 };
 
-// 询问用户
+export const TEMPLATE_LIST = {
+  vue: "vue",
+  react: "react",
+  miniApp: "miniApp",
+};
+
+console.log(Object.keys(TEMPLATE_LIST));
+
+// 命令行交互模板
 let promptList = [
   {
-    type: 'list',
-    name: 'frame',
-    message: 'please choose this project template',
-    choices: ['vue', 'react'],
+    type: "list",
+    name: "frame",
+    message: "请选择项目模板",
+    choices: Object.keys(TEMPLATE_LIST),
   },
   {
-    type: 'input',
-    name: 'description',
-    message: 'Please enter the project description: ',
+    type: "input",
+    name: "description",
+    message: "项目描述: ",
   },
   {
-    type: 'input',
-    name: 'author',
-    message: 'Please enter the author name: ',
+    type: "input",
+    name: "author",
+    message: "作者名称: ",
   },
 ];
 
-let prompt = () => {
+/**
+ * 命令行交互
+ */
+export const prompt = () => {
   return new Promise((resolve) => {
     inquirer.prompt(promptList).then((answer) => {
       resolve(answer);
@@ -49,10 +60,14 @@ let prompt = () => {
   });
 };
 
-// 项目模板远程下载
-let downloadTemplate = async (ProjectName, api) => {
+/**
+ * 项目模板远程下载
+ * @param {string} ProjectName 项目名
+ * @param {string} url 模板地址
+ */
+export const downloadTemplate = async (ProjectName, url) => {
   return new Promise((resolve, reject) => {
-    downloadGit(api, ProjectName, { clone: true }, (err) => {
+    downloadGit(`direct:${url}`, ProjectName, { clone: true }, (err) => {
       if (err) {
         reject(err);
       } else {
@@ -62,8 +77,13 @@ let downloadTemplate = async (ProjectName, api) => {
   });
 };
 
-// 更新json配置文件
-let updateJsonFile = (fileName, obj) => {
+//
+/**
+ * 更新json配置文件
+ * @param {项目名} fileName
+ * @param {*} obj
+ */
+export const updateJsonFile = (fileName, obj) => {
   return new Promise((resolve) => {
     if (fs.existsSync(fileName)) {
       const data = fs.readFileSync(fileName).toString();
@@ -71,15 +91,8 @@ let updateJsonFile = (fileName, obj) => {
       Object.keys(obj).forEach((key) => {
         json[key] = obj[key];
       });
-      fs.writeFileSync(fileName, JSON.stringify(json, null, '\t'), 'utf-8');
+      fs.writeFileSync(fileName, JSON.stringify(json, null, "\t"), "utf-8");
       resolve();
     }
   });
-};
-
-module.exports = {
-  notExistFold,
-  prompt,
-  downloadTemplate,
-  updateJsonFile,
 };
